cd21a2a2911aceb949076a30b292c4d85de70e10
AminataTambadou <Amitambadou4t@gmail.com>
2025-05-07 16:22:17 +0000

    chore: remove unnecessary files

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container py-4">
-    <h1 class="text-center mb-4">Reconnaissance Faciale</h1>
+    <h1 class="text-center mb-4">FACE RECOGNITION</h1>
     <WebcamFeed />
   </div>
 </template>
